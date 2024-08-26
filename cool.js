cbd267a6b0c0afde58bfeec23f889f98fe3e717f
jay <emma.jordan@bsu.edu>
2024-08-26 11:50:13 -0400

    creation of okay.css, edits to index.html & cool.js

--- cool.js
+++ cool.js
@@ -1,6 +1,15 @@
 let myDiv = document.getElementById("example")
 myDiv.addEventListener("click", (event) => {
-    event.target.innerHTML = "cool"
+    
+    let exampleNew = document.createElement("a")
+    exampleNew.innerText = "New Anchor"
+    event.currentTarget.appendChild(exampleNew);
+
+    event.currentTarget.classList.add("red")
+    
+    //console.log(event.currentTarget.dataset.secret)
+    //event.currentTarget.dataset.secret = "Likes chocolate"
+    //event.currentTarget.innerHTML = "cool"
 })
 
 //original statement
